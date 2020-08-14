ea2e3e3a8629a5c1ee5051e252568355887a14ef
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2020-08-14 16:25:10 -0300

    feat: adiciona view principal

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+@include('layouts.head')
+
+<body>
+    <header>
+        @include('layouts.navbar')
+    </header>
+
+
+<section class="menu">
+    <div class="row" id="menu">
+        <div class="col">Camisetas</div>
+        <div class="col">Calças</div>
+        <div class="col">Calçados</div>
+        <div class="col">Moletom</div>
+        <div class="col">Acessórios</div>
+    </div>
+</section>
+
+</body>
+
+</html>
