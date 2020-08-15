82e4f2d6ed7f722bf9ce0ef8b62e20544c5af9ef
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2020-08-15 10:15:11 -0300

    style: altera navbar

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,25 @@
+<img src="{{url('/logo/Marys.png')}}" id="logo" alt="marys">
+<nav class="navbar" id="navbar">
+    <ul class="navbar-nav mr-auto">
+        <a class="nav-link" id="nav-link" href="{{ route('produtos') }}">
+            <li id="menu-opcoes">
+                {{_('Loja')}}
+            </li>
+        </a>
+    </ul>
+    <ul class="navbar-nav mr-auto">
+        <a class="nav-link" id="nav-link" href="">
+                <li id="menu-opcoes">
+                    {{_('Minha Conta')}}
+                </li>
+            </a>
+    </ul>
+    <ul class="navbar-nav mr-auto">
+        <a class="nav-link" id="nav-link" href="">
+            <li id="menu-opcoes">
+                {{_('Carrinho')}}
+            </li>
+        </a>
+    </ul>
+</nav>
+<a class="btn btn-success" id="btn-contato" href="">{{_('Contato')}}</a>
