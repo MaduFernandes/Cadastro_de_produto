6e62f482d933de77b482bc00d4633bdaa704ea06
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2020-08-17 12:13:24 -0300

    feat: adiciona seeder usuario

---
+++ database/seeds/UsuarioSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $credenciais = [
+            'name' => 'admin',
+            'email' => 'admin@admin.com.br',
+            'password' => bcrypt('123456789'),
+        ];
+
+        if (User::where('email','=', $credenciais['email'])->count()){
+            $usuario = (User::where('email','=', $credenciais['email'])->first());
+            $usuario->save($credenciais);
+        }else{
+            User::create($credenciais);
+        }
+    }
+}
